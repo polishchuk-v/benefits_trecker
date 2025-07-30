1755f81165303005cdb606bb8f8b2f71a66fda79
dark_st <tatarinov.tima6@gmail.com>
2025-07-30 23:26:44 +0300

    додав фрагменти

---
+++ app/src/main/java/com/example/benefits_tracker/fragments/BenefitsFragment.java
@@ -0,0 +1,34 @@
+package com.example.benefits_tracker.fragments;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.benefits_tracker.R;
+
+public class BenefitsFragment extends Fragment {
+
+    public BenefitsFragment() {
+        // обов’язковий публічний конструктор
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_benefits, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view,
+                              @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Тут ініціалізується UI
+    }
+}
